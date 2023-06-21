524481ee51dc37e5410f85462b0a81f5d09b7db6
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-06-20 22:59:50 -0300

    estruturas de repetição | while, do while, for

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+var contador = 1;
+do {
+    console.log(`Passo ${contador}`);
+    contador++;
+} while(contador <=10)
+
+/*
+var contador = 1;
+while(contador <= 10) {
+    console.log(`Passo ${contador}`);
+    contador++;
+}
+*/
