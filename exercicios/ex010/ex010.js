ebcb927bf228609df53e71df20f36545b71760cd
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-06-22 15:17:22 -0300

    ex010 | registrando ações

---
+++ exercicios/ex010/ex010.js
@@ -0,0 +1,19 @@
+function clicar01() {
+    var acao = window.document.getElementById('registro');
+    acao.innerHTML += '<p>Clicou no primeiro botão.</p>';
+}
+
+function clicar02() {
+    var acao = window.document.getElementById('registro');
+    acao.innerHTML += '<p>Clicou no segundo botão.</p>';
+}
+
+function clicar03() {
+    var acao = window.document.getElementById('registro');
+    acao.innerHTML += '<p>Clicou no terceiro botão.</p>';
+}
+
+function clicar04() {
+    var acao = window.document.getElementById('registro');
+    acao.innerHTML += '<p>Clicou no quarto botão.</p>';
+}
